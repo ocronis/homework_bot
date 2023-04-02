ef772d645b86c4d2580cd7f95c2730314d2a2fea
ocronis <sorokidzes@yandex.ru>
2023-04-02 12:03:08 +0300

    test

--- homework.py
+++ homework.py
@@ -1,11 +1,15 @@
-...
+import os
+import time
+
+import telegram
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
