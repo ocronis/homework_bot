19fb4cfac20646906cb71fa0bc372c7ec5480a85
ocronis <sorokidzes@yandex.ru>
2023-04-11 10:55:17 +0300

    Sprint 7. Attempt 2

--- homework.py
+++ homework.py
@@ -91,10 +91,11 @@ def get_api_answer(current_timestamp):
 
 def check_response(response):
     """Проверяет ответ API на корректность."""
+    if not isinstance(response, dict):
+        raise TypeError('Ошибка типов')
     if not ("homeworks" in response and "current_date" in response):
         raise KeyError('Ошибка словаря')
-    if (not isinstance(response['homeworks'], list)
-            and type(response) == "<class 'dict'>"):
+    if not isinstance(response['homeworks'], list):
         raise TypeError('Ошибка типов')
     try:
         homework = (response['homeworks'])[0]
